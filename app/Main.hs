c44b7d76d05ac8abda825b157ed1cde86d0d065a
Jonas Collberg <jonas.collberg@gmail.com>
2025-04-24 11:39:00 +0200

    Uncomment code that listens for connections

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,36 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-module Main (main) where
+module Main
+  ( main
+  ) where
 
 import Control.Monad (forever)
-import qualified Data.ByteString.Char8 as BC
+import Data.ByteString.Char8 qualified as BC
 import Network.Socket
-import System.IO (BufferMode (NoBuffering), hSetBuffering, stdout, stderr)
+import System.IO (BufferMode (NoBuffering), hSetBuffering, stderr, stdout)
 
 main :: IO ()
 main = do
-    -- Disable output buffering
-    hSetBuffering stdout NoBuffering
-    hSetBuffering stderr NoBuffering
+  -- Disable output buffering.
+  hSetBuffering stdout NoBuffering
+  hSetBuffering stderr NoBuffering
 
-    -- You can use print statements as follows for debugging, they'll be visible when running tests.
-    BC.putStrLn "Logs from your program will appear here"
+  let host = "127.0.0.1"
+      port = "4221"
 
-    -- Uncomment this block to pass first stage
-    -- let host = "127.0.0.1"
-    --     port = "4221"
-    --
-    -- BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack port
-    --
-    -- -- Get address information for the given host and port
-    -- addrInfo <- getAddrInfo Nothing (Just host) (Just port)
-    --
-    -- serverSocket <- socket (addrFamily $ head addrInfo) Stream defaultProtocol
-    -- bind serverSocket $ addrAddress $ head addrInfo
-    -- listen serverSocket 5
-    --
-    -- -- Accept connections and handle them forever
-    -- forever $ do
-    --     (clientSocket, clientAddr) <- accept serverSocket
-    --     BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
-    --     -- Handle the clientSocket as needed...
-    --
-    --     close clientSocket
+  BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack port
+
+  -- Get address information for the given host and port.
+  addrInfo <- getAddrInfo Nothing (Just host) (Just port)
+
+  serverSocket <- socket (addrFamily $ head addrInfo) Stream defaultProtocol
+  bind serverSocket $ addrAddress $ head addrInfo
+  listen serverSocket 5
+
+  -- Accept connections and handle them forever
+  forever $ do
+    (clientSocket, clientAddr) <- accept serverSocket
+    BC.putStrLn $
+      "Accepted connection from "
+        <> BC.pack (show clientAddr)
+        <> "."
+
+    close clientSocket
